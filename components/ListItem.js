aae2ca24038e2e51a3c228580a1c1ddfb3e90262
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-08-11 17:30:03 +0900

    news-app

---
+++ components/ListItem.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const ListItem = ({ imageUrl, title, author, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.itemContainer} onPress={onPress}>
+      <View style={styles.leftContainer}>
+        {!!imageUrl && (
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: imageUrl }}
+          />
+        )}
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          {title}
+        </Text>
+        <Text style={styles.subText}>{author}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
+
+export default ListItem;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+import Loading from '../components/Loading';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export default function HomeScreen({ navigation }) {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      {loading && <Loading />}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
